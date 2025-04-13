3be7189c3bbb12987f0fb8b971c0542adcbd0a36
AngJianming <angjianming2005@hotmail.com>
2025-04-13 22:07:19 +0800

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract Calculator {
+    uint256 result = 0;
+
+    function get() public view returns (uint) {
+        result += num;
+    }
+}
+
+
+contract Name {
+    constructor() {
+        string memory name = "";
+    }
+}
